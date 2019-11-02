5bba39e8cf1f5648bf04feb1fe9c4d2fe376a04a
Ivan Katliarchuk <ivan.katliarchuk@gmail.com>
2019-11-02 19:00:02 +0000

    should use this instead of something specific

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "key_arn" {
   description = "The Amazon Resource Name (ARN) of the key."
-  value       = "${aws_kms_key.key.arn}"
+  value       = aws_kms_key.this.arn
 }
 
 output "key_id" {
   description = "The globally unique identifier for the key."
-  value       = "${aws_kms_key.key.id}"
+  value       = aws_kms_key.this.id
 }
 
 output "key_alias_arn" {
